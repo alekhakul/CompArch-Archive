04e03da50a92b3913aacdf009727a6ee92b9ff97
alekhakul <alekhakul@gmail.com>
2026-01-29 22:51:40 -0600

    testcase edit

--- lab2/alltest.asm
+++ lab2/alltest.asm
@@ -1,7 +1,7 @@
 .ORIG x3000
 
-;Tests all opcodes
-LEA R0, DATA        ; Load address of DATA into R0
+;Tests all instr
+LEA R0, OHKYO        ; Load address of DATA -> R0
 ADD R1, R1, #10     ; R1 <- 10
 ADD R2, R1, R1      ; R2 <- 20
 AND R3, R1, #7      ; R3 <- 10 AND 7 (2)
@@ -15,24 +15,24 @@ RSHFA R4, R4, #1    ; Arithmetic Shift Right (xFFFE)
 
 ; Word memory access
 STW R2, R0, #0      ; Store 20 at DATA[0]
-LDW R7, R0, #0      ; Load DATA[0] into R7 (Result: 20)
+LDW R7, R0, #0      ; Load DATA[0] into R7 (20)
 
 ; Byte memory access
-ADD R2, R2, #0      ; Clear R2 (for clarity)
+ADD R2, R2, #0      ; ADD 0 test
 ADD R2, R2, #-1     ; R2 <- xFFFF
 STB R2, R0, #1      ; Store xFF at DATA byte offset 1
-LDB R2, R0, #1      ; Load byte back (Result: xFFFF)
+LDB R2, R0, #1      ; Load byte back (xFFFF)
 
 ; Control flow
-BRnzp SKIP          ; Unconditional branch
-ADD R1, R1, #1      ; Should be skipped
-SKIP JSR SUB             ; Call subroutine
+BRnzp ALEX          ; Unconditional branch
+ADD R1, R1, #1      ; Should skip
+ALEX JSR SUB        ; Call subroutine
 
 HALT
 
 ; Subroutine
-SUB ADD R1, R1, #5      ; R1 <- R1 + 5
+SUB ADD R1, R1, #5  ; R1 <- R1 + 5
 RET
 
-DATA .FILL x0000
+OHKYO .FILL x0000    ; Random
 .END
